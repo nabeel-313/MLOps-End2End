a8a233bc9580c0cbb3d24cc6fa161ebc878c0012
nabeel-313 <mominnabeelahmed@gmail.com>
2024-03-02 14:56:26 +0530

    folder restructured

--- template.py
+++ template.py
@@ -1,18 +1,27 @@
 import os
 from pathlib import Path
 
-package_name = "mongodb_connect"
+#package_name = "mongodb_connect"
 
 list_of_files = [
    ".github/workflows/ci.yaml",
    "src/__init__.py",
-   f"src/{package_name}/__init__.py", 
-   f"src/{package_name}/mongo_crud.py", 
-   "tests/__init__.py",
+   "src/components/__init__.py",
+   "src/components/data_ingestion.py",
+   "src/components/data_transformation.py",
+   "src/components/model_trainer.py",
+   "src/components/model_evaluation.py", 
+   "src/pipeline/__init__.py",
+   "src/pipeline/training_pipeline.py",
+   "src/pipeline/prediction_pipeline.py",
+   "src/utils/__init__.py",
+   "src/utils/utils.py",
+   "src/logger/__init__.py",
+   "src/logger/logging.py",
+   "src/exception/__init__.py",
+   "src/exception/exception.py",
    "tests/unit/__init__.py",
-   "tests/unit/unit.py",
    "tests/integration/__init__.py",
-   "tests/integration/int.py",
    "init_setup.sh",
    "requirements.txt",
    "requirements_dev.txt", 
